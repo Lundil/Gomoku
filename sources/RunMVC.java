f84abeb62eac79e1ead0d6b68fce05f762cb7aeb
Aurelia CATRICE <catricea@meleze17.iut-infobio.priv.univ-lille1.fr>
2016-12-05 15:26:29 +0100

    okok

---
+++ sources/RunMVC.java
@@ -0,0 +1,15 @@
+import javax.swing.SwingUtilities;
+
+public class RunMVC {
+
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+            	Model model = new Model();
+                GomokuView view = new GomokuView(model); 
+                GomokuController controller = new GomokuController(model, view);
+                //controller.contol();
+		    }
+		});
+    }
+}
